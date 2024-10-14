fc8c4e66304a8905c36b98ea62b6c339ba6d212e
Gülşen Güneş <gulsenurgunes@gmail.com>
2024-10-14 21:11:14 +0300

    Added fragments to be used in submenu

---
+++ app/src/main/java/com/gulsengunes/wordmonster/ui/fragment/AddWordFragment.kt
@@ -0,0 +1,21 @@
+package com.gulsengunes.wordmonster.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gulsengunes.wordmonster.R
+
+
+class AddWordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_word, container, false)
+    }
+
+}
